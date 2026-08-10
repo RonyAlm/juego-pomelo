c8e10e0130e34d34bfe4bfb16ca297042d02ba78
Ronaldo Almirón <rony.almiron.015@gmail.com>
2026-08-10 09:02:33 -0300

    feat: add PWA support with VitePWA plugin and configure manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,43 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Juego Mintur',
+        short_name: 'Mintur',
+        description: 'Juego de preguntas y respuestas',
+        display: 'standalone',
+        background_color: '#008231',
+        start_url: '/?installed=true',
+        orientation: 'portrait',
+        theme_color: '#008231',
+        icons: [
+          {
+            src: '/icons/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      }
+    })
   ],
 })
